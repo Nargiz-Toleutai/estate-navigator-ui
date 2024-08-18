41807f8205121ce32a832b7d3c34ba91bf8dcecf
Nargiz-Toleutai <nargizabdullinova@gmail.com>
2024-08-18 21:05:24 +0200

    add basic environment config (js file)

---
+++ public/environment.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+(function (window) {
+  window.__perfectpoint_environment__ =
+    window.__perfectpoint_environment__ || {};
+  window.__perfectpoint_environment__.api_base_uri =
+    "http://localhost:3001/api";
+  window.__perfectpoint_environment__.version = "0.0.1";
+})(window);
