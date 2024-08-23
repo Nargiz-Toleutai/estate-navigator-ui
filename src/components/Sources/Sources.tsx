c6c23b434bf08ebb7f243fbd847aed3ea7f98b60
Nargiz-Toleutai <nargizabdullinova@gmail.com>
2024-08-23 15:57:22 +0200

    feat: add Sources component with corresponding styles

---
+++ src/components/Sources/Sources.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import styles from "./sources.module.scss";
+
+const Sources = () => {
+  return (
+    <section id="sources" className={styles.sources}>
+      <div className={styles.container}>
+        <div className={styles.sourcesContent}>
+          <h2>Наши партнеры</h2>
+          <ul>
+            {[
+              {
+                title: "Wildberries",
+                logo: "/media/logos/wildberries.png",
+              },
+              {
+                title: "Ozon",
+                logo: "/media/logos/ozon.png",
+              },
+              {
+                title: "Yandex Market",
+                logo: "/media/logos/yandexmarket.png",
+              },
+              {
+                title: "Avito",
+                logo: "/media/logos/avito.png",
+              },
+            ].map((source, index) => (
+              <li key={index}>
+                <div>
+                  <Image src={source.logo} alt="" width={100} height={100} />
+                </div>
+                <h3>{source.title}</h3>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Sources;
