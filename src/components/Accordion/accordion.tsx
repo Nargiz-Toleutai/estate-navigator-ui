4ba8ac5e12ef58d05e3b7955299f79091bf91aac
Nargiz-Toleutai <nargizabdullinova@gmail.com>
2024-08-23 17:24:13 +0200

    feat: update Accordion component and add styles

--- src/components/Accordion/accordion.tsx
+++ src/components/Accordion/accordion.tsx
@@ -1,21 +1,21 @@
-import {ReactNode} from "react";
-import styles from './accordion.module.scss';
-
+import { ReactNode } from "react";
+import styles from "./accordion.module.scss";
 
 export interface AccordionProps {
-    title: ReactNode;
-    children: ReactNode;
+  title: ReactNode;
+  children: ReactNode;
+  name: string;
 }
 
 export default function Accordion(props: AccordionProps) {
-    const {title, children} = props;
+  const { title, children, name } = props;
 
-    return (
-        <>
-            <details className={styles.accordion}>
-                <summary>{title}</summary>
-                {children}
-            </details>
-        </>
-    );
+  return (
+    <>
+      <details className={styles.accordion} name={name}>
+        <summary>{title}</summary>
+        {children}
+      </details>
+    </>
+  );
 }
