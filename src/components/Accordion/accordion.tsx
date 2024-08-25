0c1504910345bff2055bee86811b6cc7e5b2bd53
Nargiz-Toleutai <nargizabdullinova@gmail.com>
2024-08-25 18:10:53 +0200

    add classname props to Accordion

--- src/components/Accordion/accordion.tsx
+++ src/components/Accordion/accordion.tsx
@@ -5,14 +5,15 @@ export interface AccordionProps {
   title: ReactNode;
   children: ReactNode;
   name: string;
+  className?: string;
 }
 
 export default function Accordion(props: AccordionProps) {
-  const { title, children, name } = props;
-
+  const { title, children, name, className } = props;
+  const classNames = [className, styles.accordion].filter(Boolean).join(" ");
   return (
     <>
-      <details className={styles.accordion} name={name}>
+      <details className={classNames} name={name}>
         <summary>{title}</summary>
         {children}
       </details>
