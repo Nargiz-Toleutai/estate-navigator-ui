feaa836581cb0a1cfdae4c6b6dc9225c87fcd35f
Nargiz-Toleutai <nargizabdullinova@gmail.com>
2024-08-25 18:55:46 +0200

    add submit form logic from index to Search component

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,9 +1,28 @@
+import { checkAvitoRealEstateUrl } from "@/utils";
 import CommonButton from "../CommonButton/button";
 import LandingContainer from "../LandingContainer/LandingContainer";
 import styles from "./search.module.scss";
 import { SearchProps } from "./types";
+import { useRouter } from "next/router";
+import { FormEvent } from "react";
 
-const Search = ({ onSubmit }: SearchProps) => {
+const Search = () => {
+  const router = useRouter();
+
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const form = event.currentTarget as HTMLFormElement;
+    const formData = new FormData(form);
+    const url = formData.get("url") as string;
+
+    if (!checkAvitoRealEstateUrl(url)) {
+      alert("Некорректная ссылка на объявление в Avito");
+      return;
+    }
+
+    router.push(`/reports/${btoa(url)}`);
+  };
   return (
     <section id="search" className={styles.search}>
       <LandingContainer>
