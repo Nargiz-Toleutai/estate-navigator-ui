3abb85fedcfbbd6b905a68c8d61bf2b17f4efe2b
Nargiz-Toleutai <nargizabdullinova@gmail.com>
2024-08-27 18:21:20 +0200

    add processClassNames function

--- src/utils.ts
+++ src/utils.ts
@@ -3,3 +3,27 @@ export function checkAvitoRealEstateUrl(url: string): boolean {
     /^https:\/\/www\.avito\.ru\/.*?\/kommercheskaya_nedvizhimost\/[a-z0-9_\-.]+$/;
   return pattern.test(url);
 }
+
+type ClassItem =
+  | string
+  | (string | undefined | null)[]
+  | { [name: string]: boolean }
+  | undefined
+  | null;
+
+export function processClassNames(...items: ClassItem[]): string {
+  return items
+    .flatMap((item) => {
+      if (typeof item === "string") {
+        return item;
+      }
+      if (Array.isArray(item)) {
+        return item.filter(Boolean);
+      }
+      if (item && typeof item === "object") {
+        return Object.keys(item).filter((key) => item[key]);
+      }
+      return [];
+    })
+    .join(" ");
+}
